ad31ab1ff2ccb5ba605b3627b87e9b52d8ea6347
Андрей Куприянов <kupriyanov92@gmail.com>
2024-01-30 23:39:21 +0400

    lesson 8.2-markup

---
+++ main.js
@@ -0,0 +1,52 @@
+(function() {
+    //создаем и возвращаем заголовок приложения
+    function createAppTitle(title) {
+        let appTitle = document.createElement('h2');
+        appTitle.innerHTML = title;
+        return appTitle;
+    }
+
+    //создаем и возвращаем форму для создания дела
+    function createTodoItemForm() {
+        let form = document.createElement('form');
+        let input = document.createElement('input');
+        let buttonWrapper = document.createElement('div');
+        let button = document.createElement('button');
+
+        form.classList.add('input-group', 'mb-3');
+        input.classList.add('form-control');
+        input.placeholder = 'Введите название дела';
+        buttonWrapper.classList.add('input-group-append');
+        button.classList.add('btn', 'btn-primary');
+        button.textContent = 'Добавить дело';
+
+        buttonWrapper.append(button);
+        form.append(input);
+        form.append(buttonWrapper);
+
+        return {
+            form,
+            input,
+            button,
+        };
+    }
+
+    //создаем и возвращаем список элементов
+    function createTodoList() {
+        let list = document.createElement('ul');
+        list.classList.add('list-group');
+        return list;
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        let container = document.getElementById('todo-app');
+
+        let todoAppTitle = createAppTitle('Список дел');
+        let todoItemForm = createTodoItemForm();
+        let todoList = createTodoList();
+
+        container.append(todoAppTitle);
+        container.append(todoItemForm.form);
+        container.append(todoList);
+    })
+})();
